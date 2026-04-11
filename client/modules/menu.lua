305e5f57f0daabd883c3d38f42031f3a4e6b46f7
juddlie <271890573+juddlie@users.noreply.github.com>
2026-04-11 18:14:28 -0400

    feat(client/modules): create menu

---
+++ client/modules/menu.lua
@@ -0,0 +1,60 @@
+local config <const> = require("config")
+
+local nui <const> = require("client.modules.nui")
+local ped <const> = require("client.modules.ped")
+local camera <const> = require("client.modules.camera")
+local randomizer <const> = require("client.modules.randomizer")
+
+local menu = {}
+
+menu.active = false
+menu.originalAppearance = nil
+
+function menu.open()
+  if menu.active then return end
+
+  menu.active = true
+  menu.originalAppearance = ped.getAppearance(cache.ped)
+
+  FreezeEntityPosition(cache.ped, true)
+
+  if config.invincibleDuringCustomization then SetEntityInvincible(cache.ped, true) end
+  if config.hideRadar then DisplayRadar(false) end
+
+  camera.create()
+  
+  nui.sendMessage("setAppearance", menu.originalAppearance)
+
+  local userPresets <const> = lib.callback.await("juddlie_appearance:server:getPresets", false)
+  if userPresets then
+    nui.sendMessage("setPresets", userPresets)
+  end
+
+  nui.setVisible(true, true)
+end
+
+---@param save boolean
+function menu.close(save)
+  if not menu.active then return end
+
+  menu.active = false
+  camera.destroy()
+
+  FreezeEntityPosition(cache.ped, false)
+
+  if config.invincibleDuringCustomization then SetEntityInvincible(cache.ped, false) end
+  if config.hideRadar then DisplayRadar(true) end
+
+  ClearPedTasks(cache.ped)
+
+  SetNuiFocus(false, false)
+  nui.sendMessage("setVisible", { visible = false })
+
+  if not save then
+    ped.applyAppearance(cache.ped, menu.originalAppearance)
+  end
+
+  randomizer.stopAuto()
+end
+
+return menu
